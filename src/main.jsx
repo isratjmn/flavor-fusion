3355d86944370eca529a351f899594e0d621b602
IsratMoon <mnisrat.j@gmail.com>
2023-05-04 19:08:57 +0600

    Login Page Added

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,17 @@
-/* eslint-disable no-unused-vars */
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import { RouterProvider } from "react-router-dom";
-import router from "./routes/Routes";
-import AuthProvider from "./providers/AuthProvider";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import { RouterProvider } from 'react-router-dom';
+import router from './routes/routes/route.jsx';
+import AuthProvider from './provider/AuthProvider';
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-	<React.StrictMode>
-		<AuthProvider>
-			<RouterProvider router={router} />
-		</AuthProvider>
-	</React.StrictMode>
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    
+      <AuthProvider>
+        <RouterProvider router={router} />
+      </AuthProvider>
+    
+  </React.StrictMode>,
 );
