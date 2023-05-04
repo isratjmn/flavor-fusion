e75ee1fea1055544daac231a331d85991f23b36a
IsratMoon <mnisrat.j@gmail.com>
2023-05-04 23:48:34 +0600

    PDF File

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -8,7 +8,7 @@ const Blog = () => {
     const blogs = useLoaderData();
     return (
         <>
-            <div className="blog-banner pt-28 px-20 bg-[url('https://cdn.pixabay.com/photo/2017/09/16/16/09/sea-2755908_960_720.jpg')] h-[60vh] relative z-80 flex justify-center items-center" >
+            <div className="blog-banner pt-24 px-20 bg-[url('https://cdn.pixabay.com/photo/2017/09/16/16/09/sea-2755908_960_720.jpg')] h-[60vh] relative z-80 flex justify-center items-center" >
                 <div className='z-50'>
                     <Pdf targetRef={ref} filename="code-example.pdf">
                         {({ toPdf }) => <button onClick={toPdf} title='Download pdf' className=' btn-primary '>
